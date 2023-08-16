1ba968964cf1a6bf0d959e9b0d0b2caf42b5c5eb
vdmpeniel <vdmpeniel@yahoo.es>
2023-08-16 18:18:46 -0400

    Fixed update issues on the test code.

--- src/main/java/jdbc/SimpleJDBCRepository.java
+++ src/main/java/jdbc/SimpleJDBCRepository.java
@@ -1,6 +1,5 @@
 package jdbc;
 
-
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -16,6 +15,9 @@ public class SimpleJDBCRepository {
 
     private final Logger LOGGER = Logger.getLogger("CustomConnector");
     private final CustomDataSource ds = CustomDataSource.getInstance();
+    private PreparedStatement ps = null;
+    private Statement st = null;
+
     private final String ID = "id";
     private final String FIRST_NAME = "firstname";
     private final String LAST_NAME = "lastname";
@@ -28,25 +30,26 @@ public class SimpleJDBCRepository {
     private static final String FIND_USER_BY_NAME_SQL = "SELECT * FROM myusers WHERE firstname=?";
     private static final String FIND_ALL_USER_SQL = "SELECT * FROM myusers";
 
+    private void logMessage(String message){
+        LOGGER.info("---> " + message);
+    }
 
     public Long createUser(User user) {
         try(Connection connection = ds.getConnection()){
-            try (PreparedStatement ps = connection.prepareStatement(CREATE_USER_SQL, Statement.RETURN_GENERATED_KEYS)) {
-                ps.setLong(1, user.getId());
-                ps.setString(2, user.getFirstName());
-                ps.setString(3, user.getLastName());
-                ps.setInt(4, user.getAge());
-                logMessage(ps.toString());
+            ps = connection.prepareStatement(CREATE_USER_SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, user.getId());
+            ps.setString(2, user.getFirstName());
+            ps.setString(3, user.getLastName());
+            ps.setInt(4, user.getAge());
+            logMessage(ps.toString());
 
-                int affectedRows = ps.executeUpdate();
-                if (affectedRows > 0) {
-                    try (ResultSet generatedKeys = ps.getGeneratedKeys()) {
-                        if (generatedKeys.next()) {
-                            return generatedKeys.getLong(1);
-                        }
-                    }
-                } else { throw new SQLException("Unable to create user."); }
-            }
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows > 0) {
+                ResultSet generatedKeys = ps.getGeneratedKeys();
+                if (generatedKeys.next()) {
+                    return generatedKeys.getLong(1);
+                }
+            } else { throw new SQLException("Unable to create user."); }
 
         } catch(SQLException se){
             logMessage(se.getMessage());
@@ -56,24 +59,22 @@ public class SimpleJDBCRepository {
 
     public User findUserById(Long userId) {
         try (Connection connection = ds.getConnection()) {
-            try (PreparedStatement ps = connection.prepareStatement(FIND_USER_BY_ID_SQL)) {
-                ps.setLong(1, userId);
-                logMessage(ps.toString());
+            ps = connection.prepareStatement(FIND_USER_BY_ID_SQL);
+            ps.setLong(1, userId);
+            logMessage(ps.toString());
 
-                try(ResultSet resultset = ps.executeQuery()){
-                    if (resultset.next()) {
-                        return User.builder()
-                            .id(resultset.getLong(ID))
-                            .firstName(resultset.getString(FIRST_NAME))
-                            .lastName(resultset.getString(LAST_NAME))
-                            .age(resultset.getInt(AGE))
-                            .build();
+            ResultSet resultset = ps.executeQuery();
+            if (resultset.next()) {
+                return User.builder()
+                    .id(resultset.getLong(ID))
+                    .firstName(resultset.getString(FIRST_NAME))
+                    .lastName(resultset.getString(LAST_NAME))
+                    .age(resultset.getInt(AGE))
+                    .build();
 
-                    } else { throw new SQLException("User with id:" + userId + " not found."); }
-                }
-            }
+            } else { throw new SQLException("User with id:" + userId + " not found."); }
 
-        } catch(SQLException se){
+        }    catch(SQLException se){
             logMessage(se.getMessage());
         }
         return null;
@@ -81,21 +82,19 @@ public class SimpleJDBCRepository {
 
     public User findUserByName(String userName) {
         try (Connection connection = ds.getConnection()) {
-            try (PreparedStatement ps = connection.prepareStatement(FIND_USER_BY_NAME_SQL)) {
-                ps.setString(1, userName);
-                logMessage(ps.toString());
+            ps = connection.prepareStatement(FIND_USER_BY_NAME_SQL);
+            ps.setString(1, userName);
+            logMessage(ps.toString());
 
-                try(ResultSet resultset = ps.executeQuery()){
-                    if (resultset.next()) {
-                        return User.builder()
-                            .id(resultset.getLong(ID))
-                            .firstName(resultset.getString(FIRST_NAME))
-                            .lastName(resultset.getString(LAST_NAME))
-                            .age(resultset.getInt(AGE))
-                            .build();
-                    } else { throw new SQLException("User with name:" + userName + " not found."); }
-                }
-            }
+            ResultSet resultSet = ps.executeQuery();
+            if (resultSet.next()) {
+                return User.builder()
+                    .id(resultSet.getLong(ID))
+                    .firstName(resultSet.getString(FIRST_NAME))
+                    .lastName(resultSet.getString(LAST_NAME))
+                    .age(resultSet.getInt(AGE))
+                    .build();
+            } else { throw new SQLException("User with name:" + userName + " not found."); }
 
         } catch (SQLException se){
             logMessage(se.getMessage());
@@ -105,24 +104,22 @@ public class SimpleJDBCRepository {
 
     public List<User> findAllUser() {
         try (Connection connection = ds.getConnection()) {
-            try (Statement statement = connection.createStatement()) {
-                try (ResultSet resultSet = statement.executeQuery(FIND_ALL_USER_SQL)) {
-                    logMessage(FIND_ALL_USER_SQL);
+            st = connection.createStatement();
+            ResultSet resultSet = st.executeQuery(FIND_ALL_USER_SQL);
+            logMessage(FIND_ALL_USER_SQL);
 
-                    List<User> userList = new ArrayList<>();
-                    while(resultSet.next()){
-                        userList.add(
-                            User.builder()
-                                .id(resultSet.getLong(ID))
-                                .firstName(resultSet.getString(FIRST_NAME))
-                                .lastName(resultSet.getString(LAST_NAME))
-                                .age(resultSet.getInt(AGE))
-                                .build()
-                        );
-                    }
-                    return userList;
-                }
+            List<User> userList = new ArrayList<>();
+            while(resultSet.next()){
+                userList.add(
+                    User.builder()
+                        .id(resultSet.getLong(ID))
+                        .firstName(resultSet.getString(FIRST_NAME))
+                        .lastName(resultSet.getString(LAST_NAME))
+                        .age(resultSet.getInt(AGE))
+                        .build()
+                );
             }
+            return userList;
 
         } catch (SQLException se) { logMessage(se.getMessage()); }
         return new ArrayList<>();
@@ -130,37 +127,35 @@ public class SimpleJDBCRepository {
 
     public User updateUser(User user) {
         try(Connection connection = ds.getConnection()){
-            try(PreparedStatement ps = connection.prepareStatement(UPDATE_USER_SQL, Statement.RETURN_GENERATED_KEYS)) {
-                ps.setString(1, user.getFirstName());
-                ps.setString(2, user.getLastName());
-                ps.setInt(3, user.getAge());
-                ps.setLong(4, user.getId());
-                logMessage(ps.toString());
+            ps = connection.prepareStatement(UPDATE_USER_SQL, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, user.getFirstName());
+            ps.setString(2, user.getLastName());
+            ps.setInt(3, user.getAge());
+            ps.setLong(4, user.getId());
+            logMessage(ps.toString());
 
-                int affectedRows = ps.executeUpdate();
-                if (affectedRows > 0) {
-                    try(ResultSet generatedKeys = ps.getGeneratedKeys()) {
-                        if(generatedKeys.next()) {
-                            return findUserById(generatedKeys.getLong(1));
-                        }
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows > 0) {
+                try(ResultSet generatedKeys = ps.getGeneratedKeys()) {
+                    if(generatedKeys.next()) {
+                        return findUserById(generatedKeys.getLong(1));
                     }
-                } else { throw new SQLException("Unable to create user."); }
-            }
+                }
+            } else { throw new SQLException("Unable to create user."); }
+
         } catch (SQLException se) { logMessage(se.getMessage()); }
         return null;
-
     }
 
     public void deleteUser(Long userId) {
         try (Connection connection = ds.getConnection()) {
-            try (PreparedStatement ps = connection.prepareStatement(DELETE_USER, Statement.RETURN_GENERATED_KEYS)) {
-                ps.setLong(1, userId);
-                logMessage(ps.toString());
+            ps = connection.prepareStatement(DELETE_USER, Statement.RETURN_GENERATED_KEYS);
+            ps.setLong(1, userId);
+            logMessage(ps.toString());
 
-                int affectedRows = ps.executeUpdate();
-                if (affectedRows == 0){
-                    throw new SQLException("UserId " + userId + " not found or an error occurred. No record was deleted.");
-                }
+            int affectedRows = ps.executeUpdate();
+            if (affectedRows == 0){
+                throw new SQLException("UserId " + userId + " not found or an error occurred. No record was deleted.");
             }
 
         } catch (SQLException se) {
@@ -168,9 +163,7 @@ public class SimpleJDBCRepository {
         }
     }
 
-    private void logMessage(String query){
-        LOGGER.info("---> " + query);
-    }
+
 
 
 
